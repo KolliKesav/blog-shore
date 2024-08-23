549c65939ebcc631b41969bc3cf80703c47c4370
suryakrishnakishore <suryakrishnakishore@gmail.com>
2024-08-23 18:07:17 +0530

    Components

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import BlogPage from "./blogpage";
+import BlogShortCard from "./blogmincard";
 
 function Home()
 {
@@ -14,10 +16,18 @@ function Home()
                 </div>
                 
         </div>
-        <div >
-                <div style={{width:"30%"}}></div>
+        <div style={{display:"flex", flexDirection:"row" ,gap:"10px"}}>
+                <div style={{width:"20%" ,display:"flex",flexDirection:"column"} }>
+                    <button> <h1 className="list"> + Create a Blog</h1></button> 
+                    <button><h2 className="list">Feed</h2></button>
+                    <button><h2 className="list">My Posts</h2></button>
+                    <button><h2 className="list">Account</h2></button>
+                    
+                </div>
+                <div style={{width:"100%", marginTop:"-1rem"} } >
+           <BlogShortCard title="My Fist BLog Page" author="Kolli Kesav" date="25 aug 2024"  />
+                </div>
         </div>
-
     </div>
         
     );
